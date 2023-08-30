0ecc40bc9990b1cb48fc50d19108e9f337aae31a
qingqingxuan <596484713@qq.com>
2023-08-30 17:48:18 +0800

    adapter autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
+    autoprefixer: {
+      overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31'],
+      grid: true,
+    },
   },
 }
